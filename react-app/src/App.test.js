a0ce23307a6c65280ef340124d155b1a3fd42462
Muskan1981 <muskanagarwal3105@gmail.com>
2026-09-01 11:41:25 +0530

    Fix test to check for 'learn react' text

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -3,6 +3,5 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn abcd react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText(/learn react/i)).toBeInTheDocument();
 });
